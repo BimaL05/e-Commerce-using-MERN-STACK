dff48b5d4759dab18a09686afee1211450aa0ed3
BimaL05 <bimalk074@gmail.com>
2024-01-04 13:31:48 +0530

    Fetch API from JSON server, Filter, Sort, Pagination done

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,7 +1,42 @@
-export function fetchCount(amount = 1) {
+//function responsible for making the actual API call to fetch products.
+
+export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    const response = await fetch("https://localhost:8080");
+    //TODO: we will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products");
     const data = await response.json();
     resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort, pagination) {
+  //filter = {"category":"smartphone"}
+  //sort = {_sort:"price", _order:"desc"}
+  //pagination = {_price=1, _limit=10}
+  //TODO: multiple category support
+
+  let queryString = "";
+  for (let key in filter) {
+    const categoryValues = filter[key]; //array which is smartphone, laptops
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1]; //last value of array
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data: { products: data, totalItems: +totalItems } });
   });
 }
